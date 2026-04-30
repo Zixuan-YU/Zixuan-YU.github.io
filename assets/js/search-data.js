1023130f77c17f30b4db309f864ed351911e7aa1
Zixuan-YU <77198983+Zixuan-YU@users.noreply.github.com>
2026-04-30 16:18:52 +0000

    Deploying to gh-pages from @ Zixuan-YU/Zixuan-YU.github.io@68caea20d288aacc1241cf2f41e05c2a766d4a03 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,96 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "publications",
+          description: "selected publications",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "Selected education, experience, skills, and honors.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "news-started-phd-study-at-the-university-of-washington",
+          title: 'Started PhD study at the University of Washington.',
+          description: "",
+          section: "News",},{id: "news-published-work-in-bmc-genomics-and-journal-of-biomedical-informatics",
+          title: 'Published work in BMC Genomics and Journal of Biomedical Informatics.',
+          description: "",
+          section: "News",},{id: "news-joined-medeloop-as-a-research-engineer-intern",
+          title: 'Joined Medeloop as a Research Engineer Intern.',
+          description: "",
+          section: "News",},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%7A%69%78%75%61%6E.%6A%68%75@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Zixuan-YU", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/avery-y-b1bb71187", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=0CMZR7YAAAAJ", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
